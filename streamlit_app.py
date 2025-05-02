2138283de7e3825fda8dfd49ce84c914aa43a660
shanemacphillamy <shane.macphillamy@mantelgroup.com.au>
2025-05-02 15:20:19 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.write('The name of your smoothie will be: ', name_on_order)
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write("Choose the fruits you want in your Smoothie!")
 
-session = st.connection('snowflake').session
+session = st.connection('snowflake').session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
