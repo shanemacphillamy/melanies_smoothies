fc35c5d3af8d2ec41825eeb4f50313acb8db01a9
shanemacphillamy <shane.macphillamy@mantelgroup.com.au>
2025-05-02 15:55:50 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import snowflake.connector
+from snowflake.snowpark import Session
 
 # Access secrets
 sf_user = st.secrets["snowflake"]["user"]
@@ -12,14 +12,14 @@ sf_database = st.secrets["snowflake"]["database"]
 sf_schema = st.secrets["snowflake"]["schema"]
 
 # Connect to Snowflake
-conn = snowflake.connector.connect(
-    user=sf_user,
-    password=sf_password,
-    account=sf_account,
-    warehouse=sf_warehouse,
-    database=sf_database,
-    schema=sf_schema
-)
+connection_parameters = {
+    "account": st.secrets["snowflake"]["account"],
+    "user": st.secrets["snowflake"]["user"],
+    "password": st.secrets["snowflake"]["password"],
+    "warehouse": st.secrets["snowflake"]["warehouse"],
+    "database": st.secrets["snowflake"]["database"],
+    "schema": st.secrets["snowflake"]["schema"]
+}
 
 helpful_links = [
     "https://docs.streamlit.io",
@@ -38,7 +38,7 @@ st.write('The name of your smoothie will be: ', name_on_order)
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write("Choose the fruits you want in your Smoothie!")
 
-session = conn
+session = Session.builder.configs(connection_parameters).create()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
