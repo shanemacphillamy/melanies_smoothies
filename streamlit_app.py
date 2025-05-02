32ded25c63b750886f6a6505e9a55016947b337c
shanemacphillamy <shane.macphillamy@mantelgroup.com.au>
2025-05-02 15:48:28 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import snowflake.connector
+
+# Access secrets
+sf_user = st.secrets["snowflake"]["user"]
+sf_password = st.secrets["snowflake"]["password"]
+sf_account = st.secrets["snowflake"]["account"]
+sf_warehouse = st.secrets["snowflake"]["warehouse"]
+sf_database = st.secrets["snowflake"]["database"]
+sf_schema = st.secrets["snowflake"]["schema"]
+
+# Connect to Snowflake
+conn = snowflake.connector.connect(
+    user=sf_user,
+    password=sf_password,
+    account=sf_account,
+    warehouse=sf_warehouse,
+    database=sf_database,
+    schema=sf_schema
+)
 
 helpful_links = [
     "https://docs.streamlit.io",
@@ -19,7 +38,7 @@ st.write('The name of your smoothie will be: ', name_on_order)
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write("Choose the fruits you want in your Smoothie!")
 
-session = st.connection('snowflake').session()
+session = conn
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
