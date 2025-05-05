6f8df4416c6f188f1f6fbe9437bfdf1a31abe20c
shanemacphillamy <shane.macphillamy@mantelgroup.com.au>
2025-05-05 14:42:09 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.datframe(pd_df)
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
