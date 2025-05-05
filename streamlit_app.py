d9a7670e02166416c5e302012e9a64ac5c5f11a2
shanemacphillamy <shane.macphillamy@mantelgroup.com.au>
2025-05-05 12:22:00 +0930

    Update streamlit_app.py
    Session activation

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,7 @@ st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write("Choose the fruits you want in your Smoothie!")
 
 session = Session.builder.configs(connection_parameters).create()
+session.use_warehouse(sf_warehouse)  # 👈 Ensure warehouse is activated
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
